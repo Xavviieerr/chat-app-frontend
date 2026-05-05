a844e4ceca771d3951ed65a3686836066ceddb7a
Chidera paul ogbu <ogbupaul545@gmail.com>
2026-05-05 13:24:58 +0100

    feat: initial app setup

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import {
+	BrowserRouter as Router,
+	Routes,
+	Route,
+	Navigate,
+} from "react-router-dom";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Chat from "./pages/Chat";
+
+function App() {
+	return (
+		<AuthProvider>
+			<Router>
+				<Routes>
+					<Route path="/login" element={<Login />} />
+					<Route path="/register" element={<Register />} />
+					<Route
+						path="/"
+						element={
+							//<ProtectedRoute>
+							<Chat />
+							//</ProtectedRoute>
+						}
+					/>
+					<Route path="*" element={<Navigate to="/" />} />
+				</Routes>
+			</Router>
+		</AuthProvider>
+	);
+}
+
+export default App;
